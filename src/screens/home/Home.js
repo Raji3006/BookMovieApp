8777a02e8b2f27ceb6f8f67f8cae6d1ac2e7e564
JANNAT <rameezbanka@gmail.com>
2023-02-22 17:46:19 +0530

    added released portion

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,44 +1,47 @@
-import MovieGrid from './upcomingmovies/UpcomingMovies';
+
 import React from "react";
 import Header from "../../common/header/Header";
 import "./Home.css";
 import ReleasedMovies from './releasedmovies/ReleasedMovies';
 import { CardContent, Typography, Card, FormControl } from '@material-ui/core';
-
+import { InputLabel, Button } from '@material-ui/core';
+import UpcomingMovies from './upcomingmovies/UpcomingMovies';
 
 function Home(props) {
-    return (
-      <div>
-        <Header {...props}/>
+  return (
+    <div>
+      <Header {...props} />
 
-        <div className="upcoming-movies-heading">
-            <span>Upcoming Movies</span>
-        </div>
-        <div className="upcoming-movies">
-            <MovieGrid/>
-        </div>
-        <ReleasedMovies/>
-        
-        <div>
-          <Card>
-            <CardContent>
-              <Typography color='theme.palette.primary.light'>FIND MOVIES BY:</Typography>
-              <FormControl>
-                <InputLabel placeholder="Movie Name"></InputLabel><br/>
-                <InputLabel placeholder="Genres"></InputLabel>
-                <InputLabel placeholder="Artists"></InputLabel>
-                <InputLabel placeholder="Release Date Start"></InputLabel>
-                <InputLabel placeholder="Release Date End"></InputLabel>
-                <Button contained="variant" color="primary">APPLY</Button>
-                
-              </FormControl>
-            </CardContent>
-          </Card>
-        </div>
-
+      <div className="upcoming-movies-heading">
+        <span>Upcoming Movies</span>
       </div>
-    );
-  }
+      <div className="upcoming-movies">
+        <UpcomingMovies />
+      </div>
+      <div>
+        <ReleasedMovies />
+      </div>
+
+      <div>
+        <Card>
+          <CardContent>
+            <Typography color='theme.palette.primary.light'>FIND MOVIES BY:</Typography>
+            <FormControl>
+              <InputLabel placeholder="Movie Name"></InputLabel><br />
+              <InputLabel placeholder="Genres"></InputLabel>
+              <InputLabel placeholder="Artists"></InputLabel>
+              <InputLabel placeholder="Release Date Start"></InputLabel>
+              <InputLabel placeholder="Release Date End"></InputLabel>
+              <Button contained="variant" color="primary">APPLY</Button>
+
+            </FormControl>
+          </CardContent>
+        </Card>
+      </div>
+
+    </div>
+  );
+}
 
 
-  export default Home;
+export default Home;
--- src/screens/home/releasedmovies/ReleasedMovies.js
+++ src/screens/home/releasedmovies/ReleasedMovies.js
@@ -1,41 +1,66 @@
 import React, { useState, useEffect } from "react";
+import { ImageList, ImageListItem, ImageListItemBar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
-import GridListTileBar from "@material-ui/core/GridListTileBar";
-import "./releasedMovies.css";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    width: "76vw", 
+    margin: "16px",
+  },
+  gridList: {
+    width: "100%",
+    height: 350,
+    margin:"16px",
+  },
+  pointer: {
+    cursor: "pointer",
+  },
+}));
 
 function ReleasedMovies() {
+  const classes = useStyles();
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    fetch("api/v1/movies")
+    fetch("http://localhost:8085/api/v1/movies?page=1&limit=10&status=released")
       .then((response) => response.json())
-      .then((data) => setMovies(data))
+      .then((data) => setMovies(data.movies))
       .catch((error) => console.log(error));
   }, []);
+
+  console.log("movies:", movies);
 
   const handleMovieClick = (movieId) => {
     window.location.href = `/movie/${movieId}`;
   };
 
   return (
-    <div className="root">
-      <GridList cellHeight={350} cols={4} className="gridList">
-        {Array.isArray(movies) && movies.map((movie) => (
-          <GridListTile
+    <div className={classes.root}>
+      <ImageList rowHeight={350} cols={4} className={classes.gridList}>
+        {movies.map((movie) => (
+          <ImageListItem
             key={movie.id}
-            className="pointer"
+            className={classes.pointer}
             onClick={() => handleMovieClick(movie.id)}
           >
-            <img src={movie.poster_url} alt={movie.title} />
-            <GridListTileBar
+            {movie.poster_url ? (
+              <img src={movie.poster_url} alt={movie.title} />
+            ) : (
+              <img
+                src="https://via.placeholder.com/150x225?text=No+Poster"
+                alt={movie.title}
+              />
+            )}
+            <ImageListItemBar
               title={movie.title}
               subtitle={`Released: ${movie.release_date}`}
             />
-          </GridListTile>
+          </ImageListItem>
         ))}
-      </GridList>
+      </ImageList>
     </div>
   );
 }
