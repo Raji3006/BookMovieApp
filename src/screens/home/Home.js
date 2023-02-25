514e8204c5e78e1706a04f8deed3a761bd4e02a7
Rajeswari Behera <rajeswari.behera3006@gmail.com>
2023-02-25 11:51:26 +0530

    Added comments

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,12 +1,12 @@
-
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../common/header/Header";
 import "./Home.css";
-import ReleasedMovies from './releasedmovies/ReleasedMovies';
-import { CardContent, Typography, Card, FormControl, withStyles, Input, Select, TextField,
-  InputLabel, Button, GridList, MenuItem, Checkbox, ListItemText
-} from '@material-ui/core';
 import UpcomingMovies from './upcomingmovies/UpcomingMovies';
+import ReleasedMovies from './releasedmovies/ReleasedMovies';
+import {
+  CardContent, Typography, Card, FormControl, withStyles, Input, Select, TextField,
+  InputLabel, Button, MenuItem, Checkbox, ListItemText
+} from '@material-ui/core';
 
 const styles = (theme) => ({
   root: {
@@ -48,22 +48,22 @@ function Home(props) {
   const [genresList, setGenresList] = useState([]);
   const [artists, setArtists] = useState([]);
 
-  {/* Make API Call to Fetch the data */}
+  {/* Make API Call to Fetch the data */ }
   useEffect(() => {
     const setData = async () => {
 
-      
 
-      {/* API Call */}
+
+      {/* API Call */ }
       const getGenres = fetch(`${baseUrl}genres`);
 
-      {/* Using Promise fetch data into setGenres */}
-      const[setGenres,] = await Promise.all([
+      {/* Using Promise fetch data into setGenres */ }
+      const [setGenres,] = await Promise.all([
         getGenres,
       ]);
 
-      {/* Check whether API call was successfull and we received status as 200 */}
-      if(setGenres.status === 200){
+      {/* Check whether API call was successfull and we received status as 200 */ }
+      if (setGenres.status === 200) {
         const genres = await setGenres.json();
         setGenresList(genres.genres);
       }
@@ -71,7 +71,7 @@ function Home(props) {
     setData();
   }, []);
 
-  const changeMovieHandler = (event) =>{
+  const changeMovieHandler = (event) => {
     setMovieName(event.target.value);
   }
 
@@ -103,7 +103,6 @@ function Home(props) {
         <UpcomingMovies />
       </div>
 
-
       <div className="flex-container">
         {/* Left part to display all the release movies */}
         <div className="left">
@@ -111,78 +110,78 @@ function Home(props) {
         </div>
       </div>
 
-        {/* Right part to display the filter card */}
-        <div className="right">
-          <Card className="card">
-            <CardContent>
-              <FormControl className={classes.formControl}>
-                <Typography className={classes.title}>FIND MOVIES BY:</Typography>
-              </FormControl>
-              
-              {/* Movie Name */}
-              <FormControl className={classes.formControl}>
-                <InputLabel htmlFor="movieName">Movie Name</InputLabel>
-                <Input id="movieName" onChange={changeMovieHandler} />
-              </FormControl>
-              
-              {/* Genres */}
-              <FormControl className={classes.formControl}>
-                <InputLabel htmlFor="genres">Genres</InputLabel>
-                <Select multiple input={<Input id="genres" />}
+      {/* Right part to display the filter card */}
+      <div className="right">
+        <Card className="card">
+          <CardContent>
+            <FormControl className={classes.formControl}>
+              <Typography className={classes.title}>FIND MOVIES BY:</Typography>
+            </FormControl>
+
+            {/* Movie Name */}
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="movieName">Movie Name</InputLabel>
+              <Input id="movieName" onChange={changeMovieHandler} />
+            </FormControl>
+
+            {/* Genres */}
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="genres">Genres</InputLabel>
+              <Select multiple input={<Input id="genres" />}
                 onClick={selectGenresHandler}
-                >
+              >
                 {genresList.map((genre) => (
                   <MenuItem key={genre.id} value={genre.genre}>
                     <Checkbox checked={genres.indexOf(genre.genre) > -1} />
                     <ListItemText primary={genre.genre} />
                   </MenuItem>
                 ))}
-                </Select>
-              </FormControl>
+              </Select>
+            </FormControl>
 
-              {/* Artists */}
-              <FormControl className={classes.formControl}>
-                <InputLabel htmlFor="artists">Artists</InputLabel>
-                <Select multiple input={<Input id="artists" />}
+            {/* Artists */}
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="artists">Artists</InputLabel>
+              <Select multiple input={<Input id="artists" />}
                 onClick={selectArtistsHandler}
-                ></Select>
-              </FormControl>
+              ></Select>
+            </FormControl>
 
-              {/* Release Date Start */}
-              <FormControl className={classes.formControl}>
-                  <TextField
-                    id="releaseDateStart"
-                    label="Release Date Start"
-                    type="date"
-                    defaultValue=""
-                    InputLabelProps={{ shrink: true }}
-                    onClick={releaseDateStartHandler}
-                  />
-              </FormControl>
+            {/* Release Date Start */}
+            <FormControl className={classes.formControl}>
+              <TextField
+                id="releaseDateStart"
+                label="Release Date Start"
+                type="date"
+                defaultValue=""
+                InputLabelProps={{ shrink: true }}
+                onClick={releaseDateStartHandler}
+              />
+            </FormControl>
 
-              {/* Release Date End */}
-              <FormControl className={classes.formControl}>
-                  <TextField
-                    id="releaseDateEnd"
-                    label="Release Date End"
-                    type="date"
-                    defaultValue=""
-                    InputLabelProps={{ shrink: true }}
-                    onClick={releaseDateEndHandler}
-                  />
-              </FormControl>
+            {/* Release Date End */}
+            <FormControl className={classes.formControl}>
+              <TextField
+                id="releaseDateEnd"
+                label="Release Date End"
+                type="date"
+                defaultValue=""
+                InputLabelProps={{ shrink: true }}
+                onClick={releaseDateEndHandler}
+              />
+            </FormControl>
 
-              {/* Apply Button */}
-              <br />
-              <br />
-              <FormControl className={classes.formControl}>
-                <Button variant="contained" color="primary">APPLY</Button>
-              </FormControl>
+            {/* Apply Button */}
+            <br />
+            <br />
+            <FormControl className={classes.formControl}>
+              <Button variant="contained" color="primary">APPLY</Button>
+            </FormControl>
 
-            </CardContent>
-          </Card>
-        </div>
-      
+          </CardContent>
+        </Card>
+      </div>
+
     </div>
   );
 }
